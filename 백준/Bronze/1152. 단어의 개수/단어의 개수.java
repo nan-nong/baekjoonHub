7e6ab945475a8c5d5bb57225e458229a2957e6c7
nan-nong <128290502+nan-nong@users.noreply.github.com>
2025-01-03 21:26:31 +0900

    [Bronze II] Title: 단어의 개수, Time: 216 ms, Memory: 20100 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,14 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
+/* The Curious Case of Benjamin Button 단어의 개수*/
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 단어의 개수 세기 (공백 +1하면 안되나..?)
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+//        System.out.println(st.countTokens());
 
-        System.out.println(st.countTokens());
+        // Tokenizer class를 사용하지않고 하는 방법은?
+        String str = br.readLine();
+        int count = 0;
+        boolean prev = false;
+
+        // string을 잘라서 길이만큼 하나씩 돌리고 공백 개수 세고 +1? (안되네 ㅎ)
+        for(int i = 0 ; i < str.length() ; i++){
+            if(str.charAt(i) != ' ' && !prev){
+                prev = true;
+                count++;
+            } else if(str.charAt(i) == ' '){
+                prev = false;
+            }
+        }
+
+        System.out.println(count);
+
     }
 }
