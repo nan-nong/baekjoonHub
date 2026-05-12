d660dbf5a06214b32332aec1a7aebd77a8b36bc6
nan-nong <128290502+nan-nong@users.noreply.github.com>
2026-05-12 16:43:13 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 10.71 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        char[] phoneNumberCharArray = phone_number.toCharArray();
+
+        // 뒤에서부터 돌면서 4번째 이후부터 *로 변경
+        for (int i = phoneNumberCharArray.length - 5; i >= 0; i--) {
+            phoneNumberCharArray[i] = '*';
+        }
+        for (char c : phoneNumberCharArray) {
+            answer += c;
+        }
+
+        return answer;
+    }
+}
